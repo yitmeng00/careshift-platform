ccc3c63420995a70ecb3c38658541079210f23c6
Ivan Wong <ivanwong810520@gmail.com>
2026-04-23 10:31:11 +0800

    Add logo, install packages and config settings

--- app/clinical-scheduler-ui/eslint.config.js
+++ app/clinical-scheduler-ui/eslint.config.js
@@ -1,14 +1,38 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +44,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
--- app/clinical-scheduler-ui/vite.config.ts
+++ app/clinical-scheduler-ui/vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5172',
+        changeOrigin: true,
+      },
+      '/hubs': {
+        target: 'http://localhost:5172',
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
 })
